cd2bd218c5b84266e7f142669b26d988e27c8b2d
xuquan <thisiswoody.cs@gmail.com>
2017-08-09 14:43:25 +0800

    keras metrics callback

--- metrics/keras_callbacks.py
+++ metrics/keras_callbacks.py
@@ -1,10 +1,16 @@
 # -*- encoding: utf-8 -*-
 
-from sklearn import metrics
+"""
+Author: Woody
+Description: This is the keras callback for binary classification evaluation
+"""
 
+from sklearn import metrics
 import keras
 
-class AUC_Callback(keras.callbacks.Callback):
+import model_evaluate
+
+class Metrics_Callback(keras.callbacks.Callback):
     """Print auc at the end of each epoch, only supporting binary classification.
     """
     def on_train_begin(self, logs={}):
@@ -15,5 +21,9 @@ class AUC_Callback(keras.callbacks.Callback):
         y_pred = self.model.predict(self.validation_data[0])
         fpr, tpr, thresholds = metrics.roc_curve(self.validation_data[1], y_pred,
                                                  pos_label=1)
-        auc = metrics.auc(fpr, tpr)
-        print "At Epoch %d, the current auc is: %f" % (epoch + 1, auc)
+
+        auc, ks, opt_cut, accuracy, precision, recall = \
+        model_evaluate.model_evaluate(self.validation_data[1], y_pred)
+        print "\nAt Epoch %d, the current metrics information as follows:\nauc is: %.5f\nks is: %.5f\nopt cut \
+        is: %.5f\naccuracy is: %.5f\nprecision is: %.5f\nrecall is: %.5f\n" % (epoch + 1, auc, ks,
+                opt_cut, accuracy, precision, recall)
