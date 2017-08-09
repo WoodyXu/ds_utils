a1cfd47d05849f828fec25746a35913e6aa9453e
xuquan <thisiswoody.cs@gmail.com>
2017-08-10 00:03:04 +0800

    plotting

---
+++ metrics/plot_evaluate.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Author: Woody
+Description: This module is for plotting model evaluations, including ROC Curve, KS Curve,
+Probabilities distribution and PR Curve.
+"""
+
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
