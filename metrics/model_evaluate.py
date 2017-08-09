2b2a3e3f069a72428f6bf29dd4b932afedbf9e41
xuquan <thisiswoody.cs@gmail.com>
2017-08-09 14:34:11 +0800

    review the logic

--- metrics/model_evaluate.py
+++ metrics/model_evaluate.py
@@ -33,6 +33,7 @@ def model_evaluate(truth, pred):
 
     truth = np.asarray(truth).astype(int)
     pred = np.asarray(pred).astype(float)
+
     # auc
     fpr, tpr, thresholds = metrics.roc_curve(truth, pred, pos_label=1)
     auc = metrics.auc(fpr, tpr)
@@ -41,6 +42,7 @@ def model_evaluate(truth, pred):
     data = pd.DataFrame({"pos": truth, "pred": pred})
     data["neg"] = 1 - data["pos"]
     data["bucket"] = pd.qcut(data["pred"], 1000)
+    print data
     grouped = data.groupby('bucket', as_index=False)
 
     agg1 = pd.DataFrame()
@@ -49,12 +51,13 @@ def model_evaluate(truth, pred):
     agg1["pos_num"] = grouped.sum()["pos"]
     agg1["neg_num"] = grouped.sum()["neg"]
     agg1["total"] = agg1["pos_num"] + agg1["neg_num"]
+    print agg1
 
-    agg2 = (agg1.sort_index(by='min_pred')).reset_index(drop=True)
-    agg2["odds"] = agg2["pos_num"] * 1.0 / agg2["neg_num"]
-    agg2["neg_rate"] = agg2["neg_num"] * 1.0 / agg2["total"]
-    agg2["ks"] = np.abs((agg2["pos_num"] * 1.0 / agg2["pos_num"].sum()).cumsum() - \
-            (agg2["neg_num"] * 1.0 / agg2["neg_num"].sum()).cumsum()) * 100
+    agg2 = (agg1.sort_values(by='min_pred')).reset_index(drop=True)
+    agg2["ks"] = np.abs((agg2["neg_num"] * 1.0 / agg2["neg_num"].sum()).cumsum() - \
+            (agg2["pos_num"] * 1.0 / agg2["pos_num"].sum()).cumsum()) * 100
+
+    print agg2
 
     ks = agg2["ks"].max()
     opt_index = agg2["ks"].argmax()
@@ -66,15 +69,14 @@ def model_evaluate(truth, pred):
     fn = np.sum(np.logical_and(pred < opt_cut, truth == 1))
     tn = np.sum(np.logical_and(pred < opt_cut, truth == 0))
 
-    accuracy = (tp + tn) * 1.0 / (tp + fp + fn + tn + 0.000001)
+    accuracy = (tp + tn) * 1.0 / (tp + fp + fn + tn)
     precision = tp * 1.0 / (tp + fp + 0.000001)
-    recall = tp * 1.0 / (tp + fn + 0.000001)
+    recall = tp * 1.0 / (tp + fn + 0.0000001)
 
     return auc, ks, opt_cut, accuracy, precision, recall
 
 if __name__ == "__main__":
-    preds = np.random.random(1000)
-    truths = np.random.randint(2, size=1000)
+    preds = np.random.random(10000)
+    truths = np.random.randint(2, size=10000)
 
     print model_evaluate(truths, preds)
-    print predict_evaluate.evaluate(truths, preds)
