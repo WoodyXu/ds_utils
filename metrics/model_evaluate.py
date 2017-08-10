84ee3a83e9aeb80fe7ac1ddea00184d7a9b09329
xuquan <thisiswoody.cs@gmail.com>
2017-08-10 18:10:31 +0800

    model evaluate

--- metrics/model_evaluate.py
+++ metrics/model_evaluate.py
@@ -70,8 +70,74 @@ def model_evaluate(truth, pred):
 
     return auc, ks, opt_cut, accuracy, precision, recall
 
+def evaluate_by_segments(truth, pred, buckets=20):
+    """
+    Parameters:
+        truth: a list of ground truth, 1 or 0, neither 1 nor 0 means non-performance sample
+        pred: a list of guess probabilities, range [0, 1]
+        buckets: number of equal lengths of buckets cut in [0, 1]
+    Returns:
+        buckets segments performance, fields as follows:
+
+        score_range \t total_samples \t total samples ratio \t has_perf_samples \t
+        has_perf_samples_ratio \t neg_samples \t pos_samples \t precision \t recall
+
+        which score ranges are [0, 0.05], (0.05, 0.1], (0.1, 0.15], ..., (0.95, 1] if buckets=20 
+    Raises:
+        ValueError if both lengths of inputs are not equal
+    """
+
+    if len(truth) != len(pred):
+        raise ValueError("Lengths of truth and guesst must be equal!")
+
+    truth = np.asarray(truth).astype(int)
+    pred = np.asarray(pred).astype(float)
+    sentinel = np.asarray(["non"] * len(truth))
+
+    # add sentinels of 0 and 1
+    truth = np.append(truth, -999)
+    pred = np.append(pred, 0)
+    sentinel = np.append(sentinel, "sentinel")
+
+    truth = np.append(truth, -999)
+    pred = np.append(pred, 1)
+    sentinel = np.append(sentinel, "sentinel")
+
+    data = pd.DataFrame({"pos": truth, "pred": pred}, index=sentinel)
+    data["neg"] = 1 - data["pos"]
+    data.loc[data["pos"] != 1, "pos"] = 0
+    data.loc[data["neg"] != 1, "neg"] = 0
+    data["bucket"] = pd.cut(data["pred"], buckets)
+    data.drop("sentinel", inplace=True)
+
+    grouped = data.groupby("bucket", as_index=False)
+
+    agg = pd.DataFrame()
+    agg["bucket"] = grouped.apply(lambda x: x.name)
+    agg["total_samples"] = grouped.count()["pred"]
+    agg["pos_samples"] = grouped.sum()["pos"]
+    agg["neg_samples"] = grouped.sum()["neg"]
+    agg["has_perf_samples"] = agg["pos_samples"] + agg["neg_samples"]
+
+    agg["total_samples_ratio"] = agg["total_samples"] * 1.0 / agg["total_samples"].sum()
+    agg["has_perf_samples_ratio"] = agg["has_perf_samples"] * 1.0 / agg["has_perf_samples"].sum()
+    agg["precision"] = agg["pos_samples"] * 1.0 / agg["has_perf_samples"]
+    agg["recall"] = agg["pos_samples"] * 1.0 / agg["pos_samples"].sum()
+
+    agg = agg.sort_values(by="bucket").reset_index(drop=True)
+    agg = agg.fillna(0)
+    agg["pos_samples"] = agg["pos_samples"].astype(int)
+    agg["neg_samples"] = agg["neg_samples"].astype(int)
+    agg["has_perf_samples"] = agg["has_perf_samples"].astype(int)
+
+    return agg
+
 if __name__ == "__main__":
     preds = np.random.random(10000)
     truths = np.random.randint(2, size=10000)
 
     print model_evaluate(truths, preds)
+
+    preds = np.random.random(10000)
+    truths = np.random.randint(2, size=10000)
+    print evaluate_by_segments(truths, preds)
